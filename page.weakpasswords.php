aef1082f6300eff48417b1b1e107d2d4dccd9084
James Finstrom <jfinstrom@sangoma.com>
2015-05-26 15:46:07 -0400

    13 visual updates

--- page.weakpasswords.php
+++ page.weakpasswords.php
@@ -12,20 +12,41 @@ $action = isset($_REQUEST['action'])?$_REQUEST['action']:'';
 $email = isset($_REQUEST['email'])?$_REQUEST['email']:'';
 
 ?>
-<p>
-<?php
-	echo "<table cellpadding=5><tr><td colspan=3><div class='content'><h2>"._("Weak Password Detection")."</h2></td></tr>\n";
-	echo "<tr><td><b>"._("Type")."</b></td><td><b>"._("Name")."</b></td><td><b>"._("Secret")."</b></td><td><b>"._("Message")."</b></td></tr>";
-	$weak = weakpasswords_get_users();
-	if(sizeof($weak) > 0)  {
-		foreach ($weak as $details) {
-			echo '<tr><td>'.$details['deviceortrunk'].'</td><td>'.$details['name'].'</td><td>'.$details['secret'].'</td><td>'.$details['message']."</td></tr>";
-		}
-	} else  {
-		echo "<tr><td colspan=3>"._("No weak secrets detected on this system.")."</td></tr>";
-	}
-	// implementation of module hook
-	$module_hook = moduleHook::create();
-	echo $module_hook->hookHtml;
-?>
-	</table>
+<div class="container-fluid">
+	<h1><?php echo _('Weak Password Detection')?></h1>
+	<div class = "display full-border">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="fpbx-container">
+					<div class="display full-border">
+						<table id="weakpasswords" class="table table-striped">
+							<thead>
+								<tr>
+									<th class="col-sm-2"> <?php echo _("Type")?> </th>
+									<th class="col-sm-2"> <?php echo _("Name")?> </th>
+									<th class="col-sm-2"> <?php echo _("Secret")?> </th>
+									<th class="col-sm-6"> <?php echo _("Message")?> </th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php
+								$weak = weakpasswords_get_users();
+								if(sizeof($weak) > 0)  {
+									foreach ($weak as $details) {
+										echo '<tr><td>'.$details['deviceortrunk'].'</td><td>'.$details['name'].'</td><td>'.$details['secret'].'</td><td>'.$details['message']."</td></tr>";
+									}
+								} else  {
+									echo "<tr><td colspan=3>"._("No weak secrets detected on this system.")."</td></tr>";
+								}
+								// implementation of module hook
+								$module_hook = moduleHook::create();
+								echo $module_hook->hookHtml;
+								?>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
